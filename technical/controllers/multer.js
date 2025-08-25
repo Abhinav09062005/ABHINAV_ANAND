d949142cb9727c7bb13ebc6652e899e37b555b73
Abhinav09062005 <abhinavanandhcs@gmail.com>
2025-08-25 16:33:38 +0530

    multer

--- technical/controllers/multer.js
+++ technical/controllers/multer.js
@@ -1,26 +1,25 @@
 import multer from "multer";
 // take help from docs and google
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
+  destination: (req,file,a) => {
+    a(null, "uploads/");
   },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+  filename: (req, file, a) => {
+    a(null,Date.now()+"-"+file.originalname);
   }
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image/")) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only image files are allowed!"), false);
+const fileFilter=(req,file,a)=>{
+  if (file.mimetype.startsWith("image/")){
+    a(null,true);
+  }else{
+    a(new Error("Only image files are allowed!"),false);
   }
 };
 
-const upload = multer({
+const upload=multer({
   storage,
-  limits: { fileSize:5*1024 *1024},
+  limits: {fileSize:5*1024 *1024},
   fileFilter
 });
-
 export default upload;
